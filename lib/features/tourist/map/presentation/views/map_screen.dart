74bfa79c1e767eea2e4ca6f8cfe8ab59faddc828
williamdev20 <williamalvs45@gmail.com>
2026-09-10 15:28:22 -0300

    feat: adiciona MapLibreMap e provider de tiles

--- lib/features/tourist/map/presentation/views/map_screen.dart
+++ lib/features/tourist/map/presentation/views/map_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:passaaqui_mobile_flutter/features/tourist/map/presentation/widgets/map_header.dart';
+import 'package:maplibre_gl/maplibre_gl.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -10,6 +12,22 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  late final String mapStyleUrl;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final mapTilerKey = dotenv.env['MAPTILER_API_KEY'];
+
+    if (mapTilerKey == null || mapTilerKey.isEmpty) {
+      throw StateError("MapTiler API Key was not configured.");
+    }
+
+    mapStyleUrl =
+        "https://api.maptiler.com/maps/01a08c63-b260-733f-8081-77da900e16c0/style.json?key=$mapTilerKey";
+  }
+
   @override
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(
@@ -17,14 +35,22 @@ class _MapScreenState extends State<MapScreen> {
         statusBarColor: Colors.transparent,
         statusBarIconBrightness: Brightness.dark,
         statusBarBrightness: Brightness.light,
-        systemNavigationBarColor: Colors.black,
-        systemNavigationBarIconBrightness: Brightness.light,
+        systemNavigationBarColor: Colors.transparent,
+        systemNavigationBarIconBrightness: Brightness.dark,
         systemNavigationBarContrastEnforced: false,
       ),
       child: Scaffold(
         appBar: AppBar(
-          toolbarHeight: 65,
+          toolbarHeight: 70,
           title: MapHeader(userName: "Nome do usuario", xpAmount: 0),
+        ),
+        body: MapLibreMap(
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(-8.0675, -34.9167), // Centro de Recife
+            zoom: 17,
+            tilt: 60,
+          ),
+          styleString: mapStyleUrl,
         ),
       ),
     );
